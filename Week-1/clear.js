a61e218e4c62f45ee72363723d091d54e9e6a315
Christian Sasso <christian.sasso@gmail.com>
2016-01-21 11:43:15 -0800

    First commit.

---
+++ Week-1/clear.js
@@ -0,0 +1,13 @@
+function init() {
+	var canvas = document.getElementById("webgl-canvas");
+	gl = WebGLUtils.setupWebGL(canvas);
+	if (!gl) { return; }
+	gl.clearColor(1.0, 0.0, 1.0, 1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+function render() {
+	return;
+}
+
+window.onload = init;
