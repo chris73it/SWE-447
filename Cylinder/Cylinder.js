f69f7cabebaa328a1344a0bf49f3fb4ad4277f2b
Christian Sasso <christian.sasso@gmail.com>
2016-02-11 14:02:46 -0800

    Added Cylinder: the code is based on the Cone.

--- Cylinder/Cylinder.js
+++ Cylinder/Cylinder.js
@@ -14,7 +14,7 @@ var fshader_src =
 		gl_FragColor = v_Color;\n\
 	}';
 
-var Cone = {
+var Cylinder = {
 	positions : {
 		components : 3
 	},
@@ -91,32 +91,44 @@ var Cone = {
 			return;
 		}
 
-		// Create vertices for Cone
 		var numSides = 8;
 		var dTheta = 2.0 * Math.PI / numSides;
+
+		// Create vertices and indeces for first base of Cylinder
 		var positions = [ 0.0, 0.0, 0.0 ];  // start our positions list with the center point
 		var indices = [ 0 ]; // start with the center of the triangle fan
 		for ( var i = 0; i < numSides; ++i ) {
 			var theta = i * dTheta;
-			var x = Math.cos(theta),
-			y = Math.sin(theta),
-			z = 0.0;
+			var x = Math.cos(theta), y = Math.sin(theta), z = 0.0;
 			positions.push(x, y, z);
 			indices.push(i+1);
 		}
-		indices.push(1);  // Add in the first perimeter vertex's index to close the disk
+		indices.push(1);  // Add the first perimeter vertex's index to close the disk
 
-		positions.push(0.0, 0.0, 1.0);
+		// Create vertices and indeces for second base of Cylinder
 		indices = indices.concat(indices);
-		indices[numSides + 2] = numSides + 1;// Start with the apex vertex
-		
+		positions.push(0.0, 0.0, 1.0);
+		indices[numSides+2] = numSides + 1; // start with the center of the triangle fan
+		for (var i = 0; i < numSides; ++i) {
+			var theta = i * dTheta;
+			var x = Math.cos(theta), y = Math.sin(theta), z = 1.0;
+			positions.push(x, y, z);
+			indices[numSides + i + 3] = numSides + i + 2;
+		}
+		indices.push(numSides+2); // Add the first perimeter vertex's index to close the disk
+
+		// Create triangles for side of Cylinder
+		for (var i = 0; i < numSides; ++i) {
+			//indices.push(i+1);//FIXME
+		}
+
 		// Create a vertex buffer for vertex positions
-		Cone.positions.buffer = gl.createBuffer();
-		if (!Cone.positions.buffer) {
+		Cylinder.positions.buffer = gl.createBuffer();
+		if (!Cylinder.positions.buffer) {
 			console.log('Failed to create buffer for positions');
 			return;
 		}
-		gl.bindBuffer(gl.ARRAY_BUFFER, Cone.positions.buffer);
+		gl.bindBuffer(gl.ARRAY_BUFFER, Cylinder.positions.buffer);
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 		this.a_Position = gl.getAttribLocation(gl.program, 'a_Position');
 		if (a_Position < 0) {
@@ -125,13 +137,13 @@ var Cone = {
 		}
 
 		// Pass the colors to the vertex shader
-		Cone.colors.buffer = gl.createBuffer();
-		if (!Cone.colors.buffer) {
+		Cylinder.colors.buffer = gl.createBuffer();
+		if (!Cylinder.colors.buffer) {
 			console.log('Failed to create buffer for colors');
 			return;
 		}
-		gl.bindBuffer(gl.ARRAY_BUFFER, Cone.colors.buffer);
-		gl.bufferData(gl.ARRAY_BUFFER, Cone.colors.values, gl.STATIC_DRAW);
+		gl.bindBuffer(gl.ARRAY_BUFFER, Cylinder.colors.buffer);
+		gl.bufferData(gl.ARRAY_BUFFER, Cylinder.colors.values, gl.STATIC_DRAW);
 		this.a_Color = gl.getAttribLocation(gl.program, 'a_Color');
 		if (a_Color < 0) {
 			alert("Failed to get a_Color");
@@ -139,37 +151,37 @@ var Cone = {
 		}
 
 		// Pass the indices to the vertext shader
-		Cone.indices.buffer = gl.createBuffer();
-		if (!Cone.indices.buffer) {
+		Cylinder.indices.buffer = gl.createBuffer();
+		if (!Cylinder.indices.buffer) {
 			console.log('Failed to create buffer for indices');
 			return;
 		}
-		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Cone.indices.buffer);
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Cylinder.indices.buffer);
 		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
 
 		// Set the clear color for the canvas
 		gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-		Cone.draw();
+		Cylinder.draw();
 	},
 
 	draw : function() {
-		window.requestAnimationFrame(Cone.draw, canvas);
+		window.requestAnimationFrame(Cylinder.draw, canvas);
 
 		gl.clear(gl.COLOR_BUFFER_BIT);
 
-		gl.bindBuffer(gl.ARRAY_BUFFER, Cone.positions.buffer);
-		gl.vertexAttribPointer(a_Position, Cone.positions.components, gl.FLOAT, false, 0, 0);
+		gl.bindBuffer(gl.ARRAY_BUFFER, Cylinder.positions.buffer);
+		gl.vertexAttribPointer(a_Position, Cylinder.positions.components, gl.FLOAT, false, 0, 0);
 		gl.enableVertexAttribArray(a_Position);
 
-		gl.bindBuffer(gl.ARRAY_BUFFER, Cone.colors.buffer);
-		gl.vertexAttribPointer(a_Color, Cone.colors.components, gl.FLOAT, false, 0, 0);
+		gl.bindBuffer(gl.ARRAY_BUFFER, Cylinder.colors.buffer);
+		gl.vertexAttribPointer(a_Color, Cylinder.colors.components, gl.FLOAT, false, 0, 0);
 		gl.enableVertexAttribArray(a_Color);
 
-		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Cone.indices.buffer);
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Cylinder.indices.buffer);
 
 		// Render the base of the cone
-		var count = 10; // We'd really like Cone.baseCount, or something else clever
+		var count = 10; // We'd really like Cylinder.baseCount, or something else clever
 		var offset = 0;  // Start at the beginning of the buffer
 		gl.drawElements(gl.TRIANGLE_FAN, count, gl.UNSIGNED_SHORT, offset);
 
@@ -177,10 +189,10 @@ var Cone = {
 		count = 10;  // Same number of indices for the cone as the base
 		offset = 10 * /* sizeof(unsigned short) = */ 2;
 
-		// Draw the Cone
+		// Draw the Cylinder
 		gl.drawElements(gl.TRIANGLE_FAN, count, gl.UNSIGNED_SHORT, offset);
 	}
 };
 
-window.onload = Cone.init;
+window.onload = Cylinder.init;
 
